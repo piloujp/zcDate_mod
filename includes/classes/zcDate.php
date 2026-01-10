4c42bf4991a9f705340cb6728926c1394f0fb84e
piloujp <117885587+piloujp@users.noreply.github.com>
2026-01-11 03:07:00 +0900

    Update for ZC v2.2.0

--- includes/classes/zcDate.php
+++ includes/classes/zcDate.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Copyright 2003-2025 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Pilou2-PilouJP 2024 Mar 3 Modified in v2.0.1 $
+ * @version $Id: Pilou2-PilouJP 2026 Mar 11 Modified in v2.2.0 $
  * @since ZC v1.5.8
  */
 class zcDate extends base
